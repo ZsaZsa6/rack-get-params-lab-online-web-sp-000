ffac767e390b8bbcfd888ce0d06f3a49c3b00208
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-02 19:33:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,8 @@ class Application
       end
     end
   elsif req.path.match(/add/)
-    item_to_add = req.param
+    item_to_add = req.params["item"]
+    
       resp.write "Path Not Found"
     end
 
